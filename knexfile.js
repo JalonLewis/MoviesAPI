8559b10e8035e29e6b4cefa1f1f86e824e1bb9e4
Jalon Lewis <jalonlewis123@gmail.com>
2022-12-22 10:30:08 -0600

    bug fixes

--- knexfile.js
+++ knexfile.js
@@ -3,8 +3,7 @@ const path = require("path");
 require("dotenv").config();
 
 const {
-  //DATABASE_URL = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL = "postgres://cgtyonet:nDqyULigJZy5vqwN7Civ7knesP5gsqDF@batyr.db.elephantsql.com/cgtyonet"
+  DATABASE_URL = "postgresql://postgres@localhost/postgres",
 } = process.env;
 
 module.exports = {
